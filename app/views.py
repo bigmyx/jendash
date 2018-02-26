83451449870eaf7d2ef737501526c98ae8726edc
Michael Leikind <mleikind@cloudsimple.com>
2018-02-25 22:04:36 -0800

    more params

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
-import requests
 import time
+import requests
 from flask import render_template
 from app import app
 
@@ -11,7 +11,7 @@ def timectime(s):
 
 def _get_builds_data():
     jenkins = '{url}/job/{job}/api/json?tree=builds[number,status,timestamp,url,result,user,actions[parameters[name,value]]]'.format(url=app.config['JENKINS_URL'],
-                                                                                                                                job=app.config['JOB_NAME'])
+                                                                                                                                     job=app.config['JOB_NAME'])
     j_data = requests.get(jenkins, auth=(app.config['JENKINS_USER'], app.config['JENKINS_PASS']))
     return j_data.json()['builds']
 
@@ -19,5 +19,4 @@ def _get_builds_data():
 @app.route('/')
 def index():
     builds = _get_builds_data()
-    # import pdb; pdb.set_trace()
     return render_template("index.html", builds=builds)
