e57e08f302f48a8b052679d03b030594b0ad1fb9
Michael Leikind <mleikind@cloudsimple.com>
2018-03-04 09:01:01 -0800

    added error view, replaced bootstrap ver

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ import requests
 from flask import render_template
 from app import app
 import pdb
+import re
 
 
 @app.template_filter('ctime')
@@ -16,24 +17,43 @@ def _get_builds_data():
     j_data = requests.get(jenkins, auth=(app.config['JENKINS_USER'], app.config['JENKINS_PASS']))
 
     builds = j_data.json()['builds']
-    authors = {}
+    metadata = {}
     for build in builds:
         try:
             if build['actions'][2]['parameters'][0]['name'] == 'ENV_NAME' and build['actions'][2]['parameters'][2]['value'] == "build":
                 j_details = requests.get("{}/api/json".format(build['url']),
                                          auth=(app.config['JENKINS_USER'], app.config['JENKINS_PASS']))
+                j_console = requests.get("{}/consoleText".format(build['url']),
+                                         auth=(app.config['JENKINS_USER'], app.config['JENKINS_PASS']))
+                metadata[build['number']] = {}
+                subjob_re = 'Starting building:\s.*\s#\d+'
+                subjob_id = re.search(subjob_re, j_console.text).group(0).split(' ')[3].strip('#')
+                subjob_name = 'env-deploy-custom'
+                if build['result'] == 'FAILURE':
+                    # This call is expencive, use it only for failed builds
+                    subjob_err = requests.get("{}/job/{}/{}/consoleText".format(app.config['JENKINS_URL'],
+                                                                                subjob_name, subjob_id),
+                                              auth=(app.config['JENKINS_USER'], app.config['JENKINS_PASS']))
+                    lines = subjob_err.text.split('\n')
+                    errors = []
+                    for num, line in enumerate(lines):
+                        if "Traceback" in line:
+                            errors = lines[num - 25:num]
+
+                    metadata[build['number']]['errors'] = errors
+
                 try:
-                    authors[build['number']] = j_details.json()['actions'][0]['causes'][0]['userName']
+                    autor = j_details.json()['actions'][0]['causes'][0]['userName']
                 except KeyError:
                     autor = j_details.json()['actions'][0]['causes'][0]['upstreamProject']
-                    autor = (autor[:20] + '..') if len(autor) > 20 else autor
-                    authors[build['number']] = autor
+                    autor = (autor[:20] + '..') if len(autor) > 20 else autor  # shorten author name
+                metadata[build['number']]['author'] = autor
         except KeyError:
             pass
-    return authors, builds
+    return metadata, builds
 
 
 @app.route('/')
 def index():
-    authors, builds = _get_builds_data()
-    return render_template("index.html", builds=builds, authors=authors)
+    metadata, builds = _get_builds_data()
+    return render_template("index.html", builds=builds, metadata=metadata)
